03585401fdc5cc6d8407b849cc92a70d62aac838
LucianaErneta <luciana.erneta@hotmail.com>
2021-07-08 11:50:23 -0300

    Envio en branch

--- Paginas/Categorias.js
+++ Paginas/Categorias.js
@@ -5,7 +5,7 @@ import Encabezado from "../Componentes/Encabezado";
 const Categorias = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Encabezado title={"Categorias de pan"} />
+      <Encabezado title={"Categorias"} />
       <Button
         title="Ir a Detalle"
         onPress={() => {
