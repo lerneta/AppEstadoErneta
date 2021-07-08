c3dfbcfb2b6ed01a4551ef2ead93e00ce4ae52a3
LucianaErneta <luciana.erneta@hotmail.com>
2021-07-08 11:18:21 -0300

    commit

--- Paginas/Categorias.js
+++ Paginas/Categorias.js
@@ -5,7 +5,7 @@ import Encabezado from "../Componentes/Encabezado";
 const Categorias = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Encabezado title={"Categorias"} />
+      <Encabezado title={"Categorias2"} />
       <Button
         title="Ir a Detalle"
         onPress={() => {
