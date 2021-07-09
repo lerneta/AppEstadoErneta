b5a2bde567d7974f05db6c6bf23b202ec2011089
LucianaErneta <luciana.erneta@hotmail.com>
2021-07-08 21:17:45 -0300

    nuevo

---
+++ Componentes/mock-data.js
@@ -0,0 +1,22 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+    titulo: "Entradas",
+    color: "#f5428d",
+  },
+  {
+    id: 2,
+    titulo: "Platos Principales",
+    color: "#f54242",
+  },
+  {
+    id: 3,
+    titulo: "Bebidas",
+    color: "#f5a441",
+  },
+  {
+    id: 4,
+    titulo: "Postres",
+    color: "#f5d142",
+  },
+];
