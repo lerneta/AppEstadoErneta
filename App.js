ecb4306f8e7a5b6dd00ef44a97dd55c942db84da
LucianaErneta <luciana.erneta@hotmail.com>
2021-07-16 18:49:01 -0300

    Cambios

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
-import BreadNavigator from "./Componentes/Navegador";
+import Navigator from "./Componentes/Navegador";
 
 import store from "./Store";
 
 export default function Navegador() {
   return (
     <Provider store={store}>
-      <BreadNavigator />
+      <Navigator />
     </Provider>
   );
 }
--- Datos/Mock-data.js
+++ Datos/Mock-data.js
@@ -2,21 +2,11 @@ export const CATEGORIES = [
   {
     id: "1",
     name: "Entradas",
-    color: "#ba9593",
+    color: "#80C4CA",
   },
   {
     id: "2",
     name: "Platos principales",
-    color: "#387d7a",
-  },
-  {
-    id: "3",
-    name: "Bebidas",
-    color: "#cedfd9",
-  },
-  {
-    id: "4",
-    name: "Postre",
-    color: "#5e4c5a",
+    color: "#B980CA",
   },
 ];
